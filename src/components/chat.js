3a513809590a0e3206c72c9a20f360344c229bb8
harrison8024 <harrison8024@gmail.com>
2018-05-22 12:42:15 -0700

    chat nav home link created

---
+++ src/components/chat.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class Chat extends Component {
+    render(){
+        return (
+            <div>
+                <h1 className="center">Chat room</h1>
+            </div>
+        )
+    }
+}
+export default Chat;
---
+++ src/components/home.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default props =>(
+    <div className="center">
+        <h1>Chat Box</h1>
+        <h4>Welcome to chat bot where you can chat</h4>
+    </div>
+);
