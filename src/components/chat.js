57675be2a3c5a13decb3bf4acc09d1f2eeafcb16
harrison8024 <harrison8024@gmail.com>
2018-05-22 16:11:34 -0700

    display chat history from firebase database

--- src/components/chat.js
+++ src/components/chat.js
@@ -1,12 +1,36 @@
 import React, {Component} from 'react';
+import db from '../firebase';
+import {connect} from 'react-redux';
+import {updateChat} from '../actions';
 
 class Chat extends Component {
+    componentDidMount(){
+        db.ref('/chat-log').on('value', (snapshot) =>{
+            this.props.updateChat(snapshot.val());
+        });
+    }
     render(){
+        console.log("Chat Log", this.props.chatLog);
+        const {chatLog} = this.props;
+        const chatElements = Object.keys(chatLog).map((key,index)=>{
+            const {name, message} = chatLog[key];
+            return <li className="collection-item" key={key}><b>{name}: </b> {message}</li>
+        })
         return (
             <div>
                 <h1 className="center">Chat room</h1>
+                <ul className="collection">
+                    {chatElements}
+                </ul>
             </div>
         )
     }
 }
-export default Chat;
+
+function mapStateToProps(state){
+    return {
+        chatLog: state.chat.log
+    }
+}
+
+export default connect(mapStateToProps, {updateChat})(Chat);
